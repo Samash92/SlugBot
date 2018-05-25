8d327fde92aaa85d8baa2e4edb7cfd051c7b8f95
Samash92 <tsampson2092@gmail.com>
2018-05-25 12:03:12 -0700

    First commit: Code is mostly the example code.
    This is mostly code from the ACM example bot. Will be modified as I learn more.

---
+++ cogs/basic.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import math
+
+# discord.py calls groups of commands cogs
+# cogs can also be handlers for different types of events
+# and respond to changes in data as they happen
+
+# setup
+class BasicCog:
+    def __init__(self, bot):
+        self.bot = bot
+
+    # ping command
+    @commands.command()
+    async def ping(self, ctx):
+        # replies back to the command context with the
+        # text "Pong!"
+        await ctx.send("Pong!")
+
+# add this cog to the bot
+def setup(bot):
+    bot.add_cog(BasicCog(bot))
